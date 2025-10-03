e7ac5ba8b1332bc8daeaa2804f1329233c63979d
igorlopatka <igorlopatka@icloud.com>
2025-10-03 23:03:31 +0200

    Initial Commit

---
+++ SunRay/SunRayApp.swift
@@ -0,0 +1,17 @@
+//
+//  SunRayApp.swift
+//  SunRay
+//
+//  Created by Igor Łopatka on 03/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct SunRayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
